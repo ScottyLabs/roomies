0b56f1c1dc5aa8a12c237e094cf59e9095d9810f
Gabriel Hall <adrastopoulos@gmail.com>
2023-03-12 15:53:34 -0400

    fix 404 scroll

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -9,7 +9,7 @@ const Home: NextPage = () => {
 			<div className="fixed flex h-screen w-screen animate-pulse items-center justify-center font-mono text-[16em] tracking-tighter text-base-content text-primary-content/20 transition-all md:text-[25em]">
 				404
 			</div>
-			<div className="z-10 flex h-screen w-screen flex-col items-center justify-center gap-8 backdrop-blur">
+			<div className="fixed z-10 flex h-screen w-screen flex-col items-center justify-center gap-8 backdrop-blur">
 				<h1 className="text-5xl font-extrabold tracking-tight text-white transition-all md:text-[5rem]">
 					Where <span className="text-primary">Am</span> I?
 				</h1>
