76e44cce4e2fc17ecce65c151913a712022b028b
Gabriel Hall <adrastopoulos@gmail.com>
2023-03-12 16:22:00 -0400

    fix modal on media list

--- src/components/connections/MediaList.tsx
+++ src/components/connections/MediaList.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { Dialog, Transition } from "@headlessui/react";
 import type { Media } from "@prisma/client";
-import clsx from "clsx";
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import { toast } from "react-hot-toast";
 import {
 	FaCircleNotch,
@@ -55,7 +55,8 @@ export const MediaList: React.FC = () => {
 		}),
 	});
 
-	const [dialog, setDialog] = useState<Media | null>(null);
+	const [media, setMedia] = useState<Media | null>(null);
+	const [open, setOpen] = useState(false);
 
 	const context = trpc.useContext();
 
@@ -82,57 +83,102 @@ export const MediaList: React.FC = () => {
 
 	return (
 		<>
-			<div className={clsx("modal", dialog !== null && "modal-open")}>
-				<div className="modal-box">
-					<h3 className="text-lg font-bold">Add a Connection</h3>
-					<form
-						onSubmit={methods.handleSubmit(async (result) => {
-							await createConnection.mutateAsync({
-								provider: dialog!,
-								handle: result.handle,
-							});
-							context.connections.invalidate();
-							methods.reset();
-							setDialog(null);
-						})}
+			<Transition appear show={open} as={Fragment}>
+				<Dialog
+					as="div"
+					className="relative z-10"
+					onClose={() => setOpen(false)}
+				>
+					<Transition.Child
+						as={Fragment}
+						enter="ease-out duration-300"
+						enterFrom="opacity-0"
+						enterTo="opacity-100"
+						leave="ease-in duration-200"
+						leaveFrom="opacity-100"
+						leaveTo="opacity-0"
 					>
-						<div>
-							<label className="label flex-1">
-								<span className="label-text">
-									What is your {ProviderNames[dialog!]} handle?
-								</span>
-								<input
-									className="input-bordered input"
-									type="text"
-									{...methods.register("handle")}
-								/>
-							</label>
-							<span className="text-xs text-error">
-								{methods.formState.errors.handle?.message}
-							</span>
-						</div>
-						<div className="modal-action">
-							<button
-								className="btn-ghost btn-sm btn"
-								type="button"
-								onClick={() => setDialog(null)}
+						<div className="fixed inset-0 bg-black bg-opacity-25" />
+					</Transition.Child>
+
+					<div className="fixed inset-0 overflow-y-auto">
+						<div className="flex min-h-full items-center justify-center p-4 text-center">
+							<Transition.Child
+								as={Fragment}
+								enter="ease-out duration-300"
+								enterFrom="opacity-0 scale-95"
+								enterTo="opacity-100 scale-100"
+								leave="ease-in duration-200"
+								leaveFrom="opacity-100 scale-100"
+								leaveTo="opacity-0 scale-95"
 							>
-								Cancel
-							</button>
-							<button className="btn-secondary btn-sm btn" type="submit">
-								Submit
-							</button>
+								<Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-base-200 p-6 text-left align-middle shadow-xl transition-all">
+									<Dialog.Title
+										as="h3"
+										className="text-lg font-medium leading-6"
+									>
+										Add a Connection
+									</Dialog.Title>
+
+									<form
+										className="mt-2"
+										onSubmit={methods.handleSubmit(async (result) => {
+											await createConnection.mutateAsync({
+												provider: media!,
+												handle: result.handle,
+											});
+											context.connections.invalidate();
+											methods.reset();
+											setOpen(false);
+										})}
+									>
+										<div>
+											<label className="label flex-1 flex-col items-start gap-2">
+												<span className="label-text">
+													What is your {ProviderNames[media!]} handle?
+												</span>
+												<input
+													className="input-bordered input w-full"
+													type="text"
+													{...methods.register("handle")}
+												/>
+											</label>
+											<span className="text-xs text-error">
+												{methods.formState.errors.handle?.message}
+											</span>
+										</div>
+										<div className="modal-action">
+											<button
+												className="btn-ghost btn-sm btn"
+												type="button"
+												onClick={() => setOpen(false)}
+											>
+												Cancel
+											</button>
+											<button
+												className="btn-secondary btn-sm btn"
+												type="submit"
+											>
+												Submit
+											</button>
+										</div>
+									</form>
+								</Dialog.Panel>
+							</Transition.Child>
 						</div>
-					</form>
-				</div>
-			</div>
+					</div>
+				</Dialog>
+			</Transition>
 
 			<div className="flex gap-3">
 				{Object.values(Medias).map((data) => (
 					<button
 						key={data.provider}
-						onClick={() => setDialog(data.media)}
-						disabled={connections?.some((c) => c.provider === data.media)}
+						onClick={() => {
+							setMedia(data.media);
+							setOpen(true);
+						}}
+						disabled={connections.some((c) => c.provider === data.media)}
 						className="btn-secondary tooltip btn-square btn flex cursor-pointer items-center justify-center"
 						data-tip={data.provider}
 					>
