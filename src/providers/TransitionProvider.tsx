45f39107a38036cf877429b751accbc2ffee0fa4
Adrastopoulos <gabriel.hall@cox.net>
2023-03-19 16:58:35 -0400

    add use effect dependencies in transition layer

--- src/providers/TransitionProvider.tsx
+++ src/providers/TransitionProvider.tsx
@@ -19,17 +19,17 @@ export default function TransitionProvider({ children }: PropsWithChildren) {
 			router.events.off("routeChangeComplete", end);
 			router.events.off("routeChangeError", end);
 		};
-	}, []);
+	}, [router.events, loading]);
 
 	return (
 		<>
 			<Transition
 				appear
 				show={loading}
-				enter="transition-all duration-300"
+				enter="transition-all"
 				enterFrom="opacity-0"
 				enterTo="opacity-100"
-				leave="transition-all duration-300"
+				leave="transition-all"
 				leaveFrom="opacity-100"
 				leaveTo="opacity-0"
 				className="fixed top-0 left-0 z-50 flex h-full w-full items-center justify-center bg-base-100 bg-opacity-50 backdrop-blur"
