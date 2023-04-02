2579f265d6c62982e17870c815a273ba41f044a2
Adrastopoulos <gabriel.hall@cox.net>
2023-04-02 13:37:18 -0400

    include profile on user query

--- src/server/trpc/router/user.ts
+++ src/server/trpc/router/user.ts
@@ -5,7 +5,9 @@ export const userRouter = router({
 	getAll: publicProcedure.query(({ ctx }) => ctx.prisma.user.findMany()),
 	byId: publicProcedure
 		.input(z.object({ id: z.string() }))
-		.query(({ ctx, input }) => ctx.prisma.user.findUnique({ where: input })),
+		.query(({ ctx, input }) =>
+			ctx.prisma.user.findUnique({ where: input, include: { profile: true } })
+		),
 
 	remove: protectedProcedure
 		.input(z.object({ id: z.string() }))
