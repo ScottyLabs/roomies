cafb59886e458ab1e1fb3b1c438ff09ed7e81705
Adrastopoulos <gabriel.hall@cox.net>
2023-04-03 16:56:20 -0400

    create filter schema for profiles

--- src/server/common/schemas.tsx
+++ src/server/common/schemas.tsx
@@ -12,12 +12,12 @@ import {
 } from "@prisma/client";
 import { z } from "zod";
 
-export const ConnectionCreateSchema = z.object({
+export const ConnectionSchema = z.object({
 	provider: z.nativeEnum(Media),
 	handle: z.string(),
 });
 
-export const ProfileCreateSchema = z.object({
+export const ProfileSchema = z.object({
 	year: z.coerce
 		.number()
 		.int()
@@ -63,80 +63,45 @@ export const ProfileCreateSchema = z.object({
 	fun_fact: z.string(),
 	notes: z.string(),
 });
-
-export const ProfileUpdateSchema = z.object({
+export const ProfileUpdateSchema = ProfileSchema.partial().extend({
 	id: z.string(),
-	year: z.coerce
-		.number()
-		.int()
-		.min(new Date().getFullYear())
-		.max(new Date().getFullYear() + 4)
-		.optional(),
-	committed: z.boolean().optional(),
-	status: z.nativeEnum(Status).optional(),
-	assigned_sex: z.nativeEnum(Sex).optional(),
-	sexual_orientation: z.string().optional(),
-	pronouns: z.string().optional(),
-	location: z.string().optional(),
-	health_concerns: z.string().optional(),
-	school: z.nativeEnum(School),
-	roommate_preferred_gender: z.string().optional(),
-	roommate_preferred_schools: z.array(z.nativeEnum(School)).optional(),
-	preferred_dorm: z.array(z.nativeEnum(Dorm)).optional(),
-	bathroom_preference: z.array(z.nativeEnum(Bathroom)).optional(),
-	quiet_dorm: z.boolean().optional(),
-	wake: z
-		.string()
-		.regex(/^(0[0-9]|1[0-9]|2[0-3]):([0-5][0-9])$/)
-		.optional(),
-	time_to_ready: z.coerce
-		.number()
-		.int()
-		.min(0)
-		.max(60 * 24)
-		.optional(),
-	day_volume: z.nativeEnum(Volume).optional(),
-	sleep: z
-		.string()
-		.regex(/^(0[0-9]|1[0-9]|2[0-3]):([0-5][0-9])$/)
-		.optional(),
-	sleep_needs: z.string().optional(),
-	night_volume: z.nativeEnum(Volume).optional(),
-	snore: z.boolean().optional(),
-	shower_time: z.nativeEnum(ShowerTime).optional(),
-	study_preferences: z.string().optional(),
-	neatness: z.coerce.number().int().min(0).max(100).optional(),
-	social_energy_level: z.coerce.number().int().min(0).max(100).optional(),
-	alcohol: z.boolean().optional(),
-	drugs: z.boolean().optional(),
-	parties: z.string().optional(),
-	political_spectrum: z.string().optional(),
-	religion: z.string().optional(),
-	music: z.string().optional(),
-	aesthetic: z.string().optional(),
-	hobbies: z.string().optional(),
-	personality_test: z.string().optional(),
-	fun_fact: z.string().optional(),
-	notes: z.string().optional(),
 });
+export const ProfileSearchSchema = z
+	.object({
+		committed: z.boolean().nullable(),
+		status: z.nativeEnum(Status).nullable().or(z.literal("")),
+		assigned_sex: z.nativeEnum(Sex).nullable().or(z.literal("")),
+		school: z.nativeEnum(School).nullable().or(z.literal("")),
+		day_volume: z.nativeEnum(Volume).nullable().or(z.literal("")),
+		night_volume: z.nativeEnum(Volume).nullable().or(z.literal("")),
+		snore: z.boolean().nullable(),
+		minimum_neatness: z.coerce.number().int().min(0).max(100).nullable(),
+		minimum_social_energy_level: z.coerce
+			.number()
+			.int()
+			.min(0)
+			.max(100)
+			.nullable(),
+		alcohol: z.boolean().nullable(),
+		drugs: z.boolean().nullable(),
+	})
+	.partial();
 
-export const InvitationCreateSchema = z.object({
+export const InvitationSchema = z.object({
 	receiverId: z.string(),
 	groupId: z.string(),
 	message: z.string().max(100),
 	status: z.nativeEnum(InvitationStatus),
 });
-
 export const InvitationUpdateSchema = z.object({
 	id: z.string(),
 	status: z.nativeEnum(InvitationStatus),
 });
 
-export const MembershipCreateSchema = z.object({
+export const MembershipSchema = z.object({
 	groupId: z.string(),
 	role: z.nativeEnum(Role),
 });
-
 export const MembershipUpdateSchema = z.object({
 	id: z.string(),
 	role: z.nativeEnum(Role),
