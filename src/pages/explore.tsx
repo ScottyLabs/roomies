f3f68e33830663e7e49fcbcc1e3800ef3daf9d84
Gabriel Hall <adrastopoulos@gmail.com>
2023-04-04 14:58:39 -0400

    fix filter

--- src/pages/explore.tsx
+++ src/pages/explore.tsx
@@ -63,24 +63,50 @@ function Profiles({ query, filter }: ProfilesProps) {
 	if (status === "error") return <div>Error</div>;
 
 	const filteredProfiles = profiles.filter((profile) => {
-		if (!filter.alcohol && profile.alcohol) return false;
-		if (filter.assigned_sex && filter.assigned_sex !== profile.assigned_sex)
+		if (
+			typeof filter.alcohol !== "undefined" &&
+			!filter.alcohol &&
+			profile.alcohol
+		)
 			return false;
-		if (!filter.committed && profile.committed) return false;
-		if (filter.day_volume && filter.day_volume !== profile.day_volume)
+		if (
+			!!filter.assigned_sex &&
+			!!profile.assigned_sex &&
+			filter.assigned_sex !== profile.assigned_sex
+		)
 			return false;
-		if (!filter.drugs && profile.drugs) return false;
+		if (
+			typeof filter.committed !== "undefined" &&
+			!filter.committed &&
+			profile.committed
+		)
+			return false;
+		if (
+			!!filter.day_volume &&
+			!!profile.day_volume &&
+			filter.day_volume !== profile.day_volume
+		)
+			return false;
+		if (typeof filter.drugs !== "undefined" && !filter.drugs && profile.drugs)
+			return false;
 		if ((filter.minimum_neatness ?? -10) + 10 > profile.neatness) return false;
-		if (filter.night_volume && filter.night_volume !== profile.night_volume)
+		if (
+			!!filter.night_volume &&
+			!!profile.night_volume &&
+			filter.night_volume !== profile.night_volume
+		)
 			return false;
-		if (filter.school && filter.school !== profile.school) return false;
-		if (!filter.snore && profile.snore) return false;
+		if (!!filter.school && !!profile.school && filter.school !== profile.school)
+			return false;
+		if (typeof filter.snore !== "undefined" && !filter.snore && profile.snore)
+			return false;
 		if (
 			(filter.minimum_social_energy_level ?? -10) + 10 >
 			profile.social_energy_level
 		)
 			return false;
-		if (filter.status && filter.status !== profile.status) return false;
+		if (!!filter.status && !!profile.status && filter.status !== profile.status)
+			return false;
 
 		return (
 			profile.user.name?.toLowerCase().includes(query.toLowerCase()) ||
